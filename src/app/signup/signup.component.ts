81a8a4c471b4619eee2466fe81736682ccb76614
Meiarasu K <95343759+K-Meiarasu@users.noreply.github.com>
2022-03-01 12:57:36 +0530

    Update signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MusicserviceService } from '../musicservice.service';
 import { Router } from "@angular/router";
 
